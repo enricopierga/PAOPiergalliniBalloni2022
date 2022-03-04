99981bba3cc2b8565693bc324f52bffeb4da2a4d
enricopierga <S1093538@studenti.univpm.it>
2022-03-04 22:06:17 +0100

    prova2

--- EventBriteApp/src/main/java/it/univpm/EventBriteApp/EventBriteAppApplication.java
+++ EventBriteApp/src/main/java/it/univpm/EventBriteApp/EventBriteAppApplication.java
@@ -8,6 +8,7 @@ public class EventBriteAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventBriteAppApplication.class, args);
+		System.out.println("ico")
 	}
 
 }
